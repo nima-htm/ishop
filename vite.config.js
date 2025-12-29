2ab8449d56b90b556376835a3806ccf955ae3b10
Nima Hassani <nimahassani84@gmail.com>
2025-12-29 23:40:29 +0330

    vite configured for PWA builder

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,36 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt'],
+      manifest: {
+        name: 'ishop',
+        short_name: 'ishop',
+        start_url: '.',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#B93B7A',
+        icons: [
+          {
+            src: 'icon-192x192.svg',
+            sizes: '192x192',
+            type: 'image/svg'
+          },
+          {
+            src: 'icon-512x512.svg',
+            sizes: '512x512',
+            type: 'image/svg'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*']
+      }
+    })
+  ]
 })
